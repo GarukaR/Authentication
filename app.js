8076e4f668273dc07db0fe7850a134b4db085d52
Garuka Ranasinghem <garukar9895@gmail.com>
2020-11-01 00:29:13 +0530

    First commit - Environmental Vars V2

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model('User', userSchema);
 
